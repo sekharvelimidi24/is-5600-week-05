6ff7bf6505a7db6c2993bed54d801ff8e656825f
sekharvelimidi24 <sekhar.velimidi@slu.edu>
2025-02-17 23:07:43 +0000

    Sekhar_Velimidi_Lab_5

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
 
 
 // Set the port
@@ -19,6 +21,8 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
